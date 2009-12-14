99bc7fc82f91fae914cfe621a6e628bfeaf11ef0
Clint Webb <webb.clint@gmail.com>
2009-12-14 16:44:30 +0800

    Version 2.0, where we reduce the number of callbacks by storing the data directly and providing accessor functions.

--- src/librisp/librisp.c
+++ src/librisp/librisp.c
@@ -12,7 +12,7 @@
 #include <assert.h>
 
 
-#if (RISP_VERSION != 0x00010020)
+#if (RISP_VERSION != 0x00020000)
 #error "Incorrect header version.  code and header versions must match."
 #endif
 
@@ -27,20 +27,35 @@
 //-----------------------------------------------------------------------------
 // Initialise everything we need to initialise.   This will return a pointer to 
 // a risp_t structure that has been allocated and initialised.
-risp_t *risp_init(void)
+risp_t *risp_init(risp_t *risp)
 {
-	risp_t *risp;
-
+	risp_t *r;
+	int i;
+	
 	// if our risp_char_t type is not actually 1 byte, not sure what would happen.
 	assert(sizeof(risp_command_t) == 1);
 	assert(RISP_MAX_USER_CMD <= 256);
 
 	// allocate memory for the main struct.
-	risp = (risp_t *) malloc(sizeof(risp_t));
-	assert(risp != NULL);
-	if (risp != NULL) {
-		risp->invalid = NULL;
-		memset(risp->commands, 0, (RISP_MAX_USER_CMD*sizeof(void *)));
+	if (risp == NULL) {
+		r = (risp_t *) malloc(sizeof(risp_t));
+		r->created_internally = 1;
+	}
+	else {
+		r = risp;
+		r->created_internally = 0;
+	}
+	
+	assert(r != NULL);
+	if (r != NULL) {
+		for (i=0; i<RISP_MAX_USER_CMD; i++) {
+			r->commands[i].handler = NULL;
+			r->commands[i].set = 0;
+			r->commands[i].length = 0;
+			r->commands[i].max = 0;
+			r->commands[i].buffer = NULL;
+			r->commands[i].value = 0;
+		}
 	}
 	
 	return(risp);
@@ -52,9 +67,104 @@ risp_t *risp_init(void)
 // Clean up the structure that were created by the library.  
 void risp_shutdown(risp_t *risp)
 {
+	int i;
+	
 	assert(risp != NULL);
-	memset(risp->commands, 0, (RISP_MAX_USER_CMD*sizeof(void *)));
+	for (i=0; i<RISP_MAX_USER_CMD; i++) {
+		risp->commands[i].handler = NULL;
+		risp->commands[i].set = 0;
+		if (risp->commands[i].buffer) {
+			risp->commands[i].length = 0;
+			risp->commands[i].max = 0;
+			free(risp->commands[i].buffer);
+			risp->commands[i].buffer = NULL;
+		}
+	}
+	
+	assert(risp->created_internally == 1 | risp->created_internally == 0);
+	if (risp->created_internally == 1) {
+		free(risp);
+	}
 }
+
+
+
+//-----------------------------------------------------------------------------
+// This function is used to reduce the amount of memory that is used in 
+// buffers.  It goes through the array, and resizes all memor buffers to the 
+// current length.  This means that if a buffer was allocated, but is currently 
+// empty, it will be deallocated.
+void risp_flush(risp_t *risp)
+{
+	int i;
+	
+	for (i=0; i<RISP_MAX_USER_CMD; i++) {
+		
+		// we only care about commands that dont have a handler, because if it 
+		// had a handler, then we wouldn't be buffering data.
+		if (risp->commands[i].handler == NULL) {
+			
+			// if there is currently a buffer set.
+			if (risp->commands[i].buffer) {
+				assert(risp->commands[i].max > 0);
+				
+				if (risp->commands[i].set > 0) {
+					// the buffer currently has live data in it, so we can only 
+					// reduce it, and cannot free all of it.
+					
+					if (risp->commands[i].length < risp->commands[i].max) {
+						risp->commands[i].buffer = realloc(risp->commands[i].buffer, risp->commands[i].length);
+						assert(risp->commands[i].buffer);
+						risp->commands[i].max = risp->commands[i].length;
+					}
+				}
+				else {
+					// the buffer is not currently set, so we can free the 
+					// entire thing.
+					
+					risp->commands[i].max = 0;
+					risp->commands[i].length = 0;
+					free(risp->commands[i].buffer);
+					risp->commands[i].buffer = NULL;
+					risp->commands[i].value = 0;
+				}
+			}
+		}
+	}
+}
+
+
+
+//-----------------------------------------------------------------------------
+// This function will clear all the buffered values.
+void risp_clear_all(risp_t *risp)
+{
+	int i;
+	
+	assert(risp);
+	
+	for (i=0; i<RISP_MAX_USER_CMD; i++) {
+		if (risp->commands[i].set > 0) {
+			risp->commands[i].set = 0;
+			risp->commands[i].length = 0;
+			risp->commands[i].value = 0;
+		}
+	}
+}
+
+
+//-----------------------------------------------------------------------------
+// reset the data for a particular command.  It does not dissolve any memory 
+// allocated.  It merely resets the length and 'set' values;
+void risp_clear(risp_t *risp, risp_command_t command)
+{
+	assert(risp);
+	assert(risp->commands[command].handler == NULL);
+	risp->commands[command].set = 0;
+	risp->commands[command].length = 0;
+	risp->commands[command].value = 0;
+}
+
 
 
 //-----------------------------------------------------------------------------
@@ -69,28 +179,13 @@ void risp_add_command(risp_t *risp, risp_command_t command, void *callback)
 	assert(command < RISP_MAX_USER_CMD);
 	assert(callback != NULL);
 	
-	assert(risp->commands != NULL);
-	assert(risp->commands[command] == NULL);
-	
-	if (risp->commands[command] == NULL) {
-		risp->commands[command] = callback;
-	}
-}
-
-
-
-//-----------------------------------------------------------------------------
-// Add a command to our tables.  Since we are using an array of function 
-// pointers, risp does not know definitively that the function specified 
-// expects the correct parameters.  If the callback function is not the correct 
-// type for the command-style, then it will generally end up with a segfault.
-void risp_add_invalid(risp_t *risp, void *callback) 
-{
-	assert(risp != NULL);
-	assert(callback != NULL);
-	
-	assert(risp->invalid == NULL);
-	risp->invalid = callback;
+	assert(risp->commands[command].handler == NULL);
+	risp->commands[command].handler = callback;
+	risp->commands[command].set = 0;
+	risp->commands[command].length = 0;
+	risp->commands[command].max = 0;
+	risp->commands[command].buffer = NULL;
+	risp->commands[command].value = 0;
 }
 
 
@@ -113,7 +208,6 @@ risp_length_t risp_process(risp_t *risp, void *base, risp_length_t len, const vo
 	void (*func_nul)(void *base) = NULL;
 	void (*func_int)(void *base, const risp_int_t value) = NULL;
 	void (*func_str)(void *base, const risp_length_t length, const void *data) = NULL;
-	void (*func_inv)(void *base, const void *data, const risp_length_t length) = risp->invalid;
 	
 	
 // 	assert(risp != NULL);
@@ -138,9 +232,9 @@ risp_length_t risp_process(risp_t *risp, void *base, risp_length_t len, const vo
 			case 1:
 				// 0 to 63			No param
 				
-				func_nul = risp->commands[cmd];
+				func_nul = risp->commands[cmd].handler;
 				if (func_nul) 		 (*func_nul)(base);
-				else if (func_inv) (*func_inv)(base, ptr, left);
+				else { risp->commands[cmd].set = 1; }
 				ptr++;
 				left--;
 				break;
@@ -148,12 +242,15 @@ risp_length_t risp_process(risp_t *risp, void *base, risp_length_t len, const vo
 			case 2:
 				// 64 to 95		1 byte param
 				if (left > 1) {
-					func_int = risp->commands[cmd];
+					func_int = risp->commands[cmd].handler;
 					if (func_int) {
 						value = (unsigned char) ptr[1];
 						(*func_int)(base, value);
 					}
-					else if (func_inv) (*func_inv)(base, ptr, left);
+					else {
+						risp->commands[cmd].set = 1;
+						risp->commands[cmd].value = value; 
+					}
 					ptr += 2;
 					left -= 2;
 				}
@@ -163,13 +260,16 @@ risp_length_t risp_process(risp_t *risp, void *base, risp_length_t len, const vo
 			case 3:
 				// 96 to 127		2 byte param
 				if (left > 2) {
-					func_int = risp->commands[cmd];
+					func_int = risp->commands[cmd].handler;
 					if (func_int) {
 						value = ((unsigned char) ptr[1] << 8) + 
 									  ((unsigned char) ptr[2]);
 						(*func_int)(base, value);
 					}
-					else if (func_inv) (*func_inv)(base, ptr, left);
+					else {
+						risp->commands[cmd].set = 1;
+						risp->commands[cmd].value = value; 
+					}
 					ptr += 3;
 					left -= 3;
 				}
@@ -179,7 +279,7 @@ risp_length_t risp_process(risp_t *risp, void *base, risp_length_t len, const vo
 			case 4:
 				// 128 to 159		4 byte param
 				if (left > 4) {
-					func_int = risp->commands[cmd];
+					func_int = risp->commands[cmd].handler;
 					if (func_int) {
 						value = ((unsigned char) ptr[1] << 24) +
 										((unsigned char) ptr[2] << 16) +
@@ -187,7 +287,10 @@ risp_length_t risp_process(risp_t *risp, void *base, risp_length_t len, const vo
 										((unsigned char) ptr[4]);
 						(*func_int)(base, value);
 					}
-					else if (func_inv) (*func_inv)(base, ptr, left);
+					else {
+						risp->commands[cmd].set = 1;
+						risp->commands[cmd].value = value; 
+					}
 					ptr += 5;
 					left -= 5;
 				}
@@ -199,9 +302,17 @@ risp_length_t risp_process(risp_t *risp, void *base, risp_length_t len, const vo
 				if (left > 1) {
 					length = ptr[1];
 					if (left > 1 + length) {
-						func_str = risp->commands[cmd];
+						func_str = risp->commands[cmd].handler;
 						if (func_str) 		 (*func_str)(base, length, ptr+2);
-						else if (func_inv) (*func_inv)(base, ptr, left);
+						else {
+							if (risp->commands[cmd].max < length) {
+								risp->commands[cmd].max = length;
+								risp->commands[cmd].buffer = realloc(risp->commands[cmd].buffer, length);
+							}
+							memcpy(risp->commands[cmd].buffer, ptr+2, length);
+							risp->commands[cmd].length = length;
+							risp->commands[cmd].set = 1;
+						}
 						ptr += (2 + length);
 						left -= (2 + length);
 					}
@@ -216,9 +327,17 @@ risp_length_t risp_process(risp_t *risp, void *base, risp_length_t len, const vo
 					length = ((unsigned char) ptr[1] << 8) + 
 								   ((unsigned char) ptr[2]);
 					if (left > 2 + length) {
-						func_str = risp->commands[cmd];
+						func_str = risp->commands[cmd].handler;
 						if (func_str)      (*func_str)(base, length, ptr+3);
-						else if (func_inv) (*func_inv)(base, ptr, left);
+						else {
+							if (risp->commands[cmd].max < length) {
+								risp->commands[cmd].max = length;
+								risp->commands[cmd].buffer = realloc(risp->commands[cmd].buffer, length);
+							}
+							memcpy(risp->commands[cmd].buffer, ptr+3, length);
+							risp->commands[cmd].length = length;
+							risp->commands[cmd].set = 1;
+						}
 						ptr += (3 + length);
 						left -= (3 + length);
 					}
@@ -235,9 +354,17 @@ risp_length_t risp_process(risp_t *risp, void *base, risp_length_t len, const vo
 									 ((unsigned char) ptr[3] << 8) +
 									 ((unsigned char) ptr[4]);
 					if (left > 4 + length) {
-						func_str = risp->commands[cmd];
+						func_str = risp->commands[cmd].handler;
 						if (func_str) 	   (*func_str)(base, length, ptr+5);
-						else if (func_inv) (*func_inv)(base, ptr, left);
+						else {
+							if (risp->commands[cmd].max < length) {
+								risp->commands[cmd].max = length;
+								risp->commands[cmd].buffer = realloc(risp->commands[cmd].buffer, length);
+							}
+							memcpy(risp->commands[cmd].buffer, ptr+5, length);
+							risp->commands[cmd].length = length;
+							risp->commands[cmd].set = 1;
+						}
 						ptr += (5 + length);
 						left -= (5 + length);
 					}
@@ -259,3 +386,40 @@ risp_length_t risp_process(risp_t *risp, void *base, risp_length_t len, const vo
 
 	return(len - left);
 }
+
+
+// return 0 if not set, 1 if it is.
+int risp_isset(risp_t *risp, risp_command_t command)
+{
+	assert(risp);
+	assert(risp->commands[command].handler == NULL);
+	return(risp->commands[command].set);
+}
+
+
+int risp_getvalue(risp_t *risp, risp_command_t command)
+{
+	assert(risp);
+	assert(risp->commands[command].handler == NULL);
+	return(risp->commands[command].value);
+}
+
+unsigned int risp_getlength(risp_t *risp, risp_command_t command)
+{
+	assert(risp);
+	assert(risp->commands[command].handler == NULL);
+	assert(risp->commands[command].length <= risp->commands[command].max);
+	return(risp->commands[command].length);
+}
+
+
+char * risp_getdata(risp_t *risp, risp_command_t command)
+{
+	assert(risp);
+	assert(risp->commands[command].handler == NULL);
+	assert(risp->commands[command].length <= risp->commands[command].max);
+	assert(risp->commands[command].buffer);
+	return(risp->commands[command].buffer);
+}
+
+
--- src/librisp/risp.h
+++ src/librisp/risp.h
@@ -10,8 +10,20 @@
 #ifndef __LIBRISP_H
 #define __LIBRISP_H
 
-#define RISP_VERSION 0x00010020
-#define RISP_VERSION_NAME "v1.00.20"
+
+/*
+	Version 1.x did not buffer anything.  Instead it only supported callbacks.  
+	With this version, you only need to set a callback for commands that 
+	require an action, otherwise the content of the commands that do not have 
+	callbacks will be stored and retrieved when needed.   The retreival can be 
+	done in a macro, which means that it can be optimised for faster access 
+	(normally just a pointer redirection).
+*/
+
+
+
+#define RISP_VERSION 0x00020000
+#define RISP_VERSION_NAME "v2.00.00"
 
 
 #define RISP_MAX_USER_CMD    256
@@ -24,9 +36,17 @@ typedef unsigned int    risp_length_t;
 typedef int             risp_int_t;
 typedef unsigned char   risp_data_t;
 
+
 typedef struct {
-	void *invalid;
-	void *commands[RISP_MAX_USER_CMD];
+	struct {
+		void *handler;
+		char set;
+		unsigned int max;
+		unsigned int length;
+		unsigned char *buffer;
+		int value;
+	} commands[RISP_MAX_USER_CMD];
+	char created_internally;
 } risp_t;
 
 
@@ -35,14 +55,24 @@ typedef struct {
 // declare the public functions.
 
 // init and shutdown.
-risp_t *risp_init(void);
+risp_t *risp_init(risp_t *risp);
 void risp_shutdown(risp_t *risp);
+
+void risp_flush(risp_t *risp);
+void risp_clear(risp_t *risp, risp_command_t command);
+void risp_clear_all(risp_t *risp);
+
 
 // setup of callback commands
 void risp_add_command(risp_t *risp, risp_command_t command, void *callback);
-void risp_add_invalid(risp_t *risp, void *callback);
 
 // providing data that needs to be processed and sent to the callback commands.  Will return the number of bytes that were processed.
 risp_length_t risp_process(risp_t *risp, void *base, risp_length_t length, const void *data);
+
+// these functions should be converted to macro's or inlined somehow to improve efficiency.
+int risp_isset(risp_t *risp, risp_command_t command);
+int risp_getvalue(risp_t *risp, risp_command_t command);
+unsigned int risp_getlength(risp_t *risp, risp_command_t command);
+char * risp_getdata(risp_t *risp, risp_command_t command);
 
 #endif
